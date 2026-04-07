be36b1a2f4f9b67cb5a22182f281f063fc76f67d
omaryoussef160 <omaryoussef150@gmail.com>
2026-04-07 14:43:31 +0200

    fix vercel

--- task node/api/index.js
+++ task node/api/index.js
@@ -1,19 +1,22 @@
-require("dotenv").config();
-const connectDB = require("../config/db");
-const app = require("../app");
+// require("dotenv").config();
+// const connectDB = require("../config/db");
+// const app = require("../app");
 
-let isConnected = false;
+// let isConnected = false;
 
-module.exports = async (req, res) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  // ✅ بدل app(req, res)
-  return new Promise((resolve, reject) => {
-    app(req, res, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-};
+// module.exports = async (req, res) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
+
+//   return new Promise((resolve, reject) => {
+//     app(req, res, (err) => {
+//       if (err) reject(err);
+//       else resolve();
+//     });
+//   });
+// };
+import app from "../app.js";
+
+export default app;
