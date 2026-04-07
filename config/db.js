e64f57f5a3e5d5442366433f56dac2527a429594
omaryoussef160 <omaryoussef150@gmail.com>
2026-04-07 15:28:23 +0200

    final fix

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (cached.conn) return cached.conn;
 
-  const db = await mongoose.connect(process.env.MONGO_URI);
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI);
+  }
 
-  isConnected = db.connections[0].readyState;
-  console.log("MongoDB connected");
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
