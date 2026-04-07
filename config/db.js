13293e2b638d65a141866faf322b2d52dcf2b9a6
omaryoussef160 <omaryoussef150@gmail.com>
2026-04-07 15:22:30 +0200

    final fix

--- config/db.js
+++ config/db.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
 
-let connecting;
+let isConnected = false;
 
-async function connectDB() {
-  const uri = process.env.MONGODB_URI || process.env.MONGO_URL;
-  if (!uri) {
-    throw new Error("MONGODB_URI (or MONGO_URL) is not set");
-  }
-  if (mongoose.connection.readyState === 1) return;
-  if (connecting) return connecting;
+const connectDB = async () => {
+  if (isConnected) return;
 
-  connecting = mongoose.connect(uri);
-  await connecting;
-  connecting = null;
-}
+  const db = await mongoose.connect(process.env.MONGO_URI);
+
+  isConnected = db.connections[0].readyState;
+  console.log("MongoDB connected");
+};
 
 module.exports = connectDB;
